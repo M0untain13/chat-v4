9fa57be77147cfbaeb29d0c55c9252f70afb932e
Золотарёв Данил <zolotariov.danil@yandex.ru>
2023-11-13 11:17:55 +0500

    написал инициализацию сервера и клиента в консолях

--- ClientConsole/ClientConsole.cs
+++ ClientConsole/ClientConsole.cs
@@ -1,11 +1,13 @@
-﻿namespace ClientConsole
+﻿using NetArc.Client;
+
+namespace ClientConsole
 {
     internal class ClientConsole
     {
         // TODO: Создать клиент, запустить клиент, отправлять сообщения серверу
         private static void Main()
         {
-            
+            var client = new Client(Callback, 8000, 8001);
         }
 
         // TODO: Метод для получения сообщений с сервера
--- ServerConsole/ServerConsole.cs
+++ ServerConsole/ServerConsole.cs
@@ -1,11 +1,13 @@
-﻿namespace ServerConsole
+﻿using NetArc.Server;
+
+namespace ServerConsole
 {
     internal class ServerConsole
     {
         // TODO: Создать сервер, запустить сервер, отправлять сообщения клиентам
         private static void Main()
         {
-
+            var server = new Server(Callback, 8000, 8001, 2000);
         }
 
         // TODO: Метод для получения сообщений с клиентов
