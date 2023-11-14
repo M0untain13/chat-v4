13c0d4f4beac86593bbcabf8d7ebf60f44a2c00c
Золотарёв Данил <zolotariov.danil@yandex.ru>
2023-11-14 12:36:39 +0500

    Я на 100% не уверен, но вьюмодели для приложения готовы

--- ClientCore/ViewModels/ChatViewModel.cs
+++ ClientCore/ViewModels/ChatViewModel.cs
@@ -4,17 +4,53 @@ using ClientCore.Services;
 using MvvmCross.ViewModels;
 using NetArc;
 using System.Xml.Linq;
+using MvvmCross.Commands;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace ClientCore.ViewModels
 {
     public class ChatViewModel : MvxViewModel<(IClientWrapper, string)>
     {
+        #region Коллекция пришедших сообщений
+
         private ObservableCollection<(string, string)> _messages = new();
         public ObservableCollection<(string, string)> Messages
         {
             get => _messages;
             set => SetProperty(ref _messages, value);
+        }
+
+        #endregion
+
+        #region Поле ввода сообщения
+
+        private string _text = string.Empty;
+        public string Text
+        {
+            get => _text;
+            set => SetProperty(ref _text, value);
+        }
+
+        #endregion
+
+        #region Команды
+
+        public IMvxCommand SendCommand { get; }
+
+        #endregion
+
+        public ChatViewModel()
+        {
+            #region Инициализация команд
+
+            SendCommand = new MvxCommand(
+                () => {
+                    _client.Send($"{Tag.MESSAGE}{Tag.Wrap(_name, Tag.NAME_S, Tag.NAME_E)}{Tag.Wrap(_text, Tag.TEXT_S, Tag.TEXT_E)}");
+                },
+                () => _text.Length != 0
+            );
+
+            #endregion
         }
 
         public override void Prepare((IClientWrapper, string) client)
@@ -24,7 +60,7 @@ namespace ClientCore.ViewModels
             _client.SetCallback(_Callback);
         }
 
-        private IClientWrapper? _client;
+        private IClientWrapper _client = null!;
         private string _name = string.Empty;
 
         private void _Callback(string message)
