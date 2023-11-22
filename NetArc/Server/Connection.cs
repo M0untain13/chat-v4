88e17bfc7e8bd92f3df9684f013ec91705a4426e
Данил Золотарёв <dan2002.dan2002@mail.ru>
2023-11-22 21:03:54 +0500

    Реализовал класс коннекта сервера с клиентом

--- NetArc/Server/Connection.cs
+++ NetArc/Server/Connection.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using NetArc.Client;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -14,46 +15,34 @@ internal class Connection
     /// </summary>
     /// <param name="client"> Сокет клиента </param>
     /// <param name="callback"> Метод для обработки сообщений клиента </param>
-    
-    private static int SERVERUDPPORT = 8000;
-    private Socket _UdpSocket;
     public Connection(Socket client, Action<WebMessage> callback)
     {
-        _UdpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-        _UdpSocket.Bind(new IPEndPoint(IPAddress.Any, SERVERUDPPORT));
-
-        Parser parser_mes = new Parser();
-
-        Task UDPThread = new Task(() =>
-        {
-            try
-            {
-                while (true)
-                {
-                    EndPoint clientEP = new IPEndPoint(IPAddress.Any, 0);
-                    //int bytesRead = _UdpSocket.ReceiveFrom(buffer, ref clientEP);
-
-                    //string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                    //callback(parser_mes.ParseMessage(message));
-                    //Console.WriteLine("Получено от клиента: " + message);
-                }
-            }
-            catch (SocketException ex)
-            {
-                Console.WriteLine($"Error while receiving the message: {ex.Message}");
-            }
-        });
+        _client = client;
+        _callback = callback;
+        _parser = new Parser();
     }
-
-   
-
 
     /// <summary>
     /// Запустить прослушку входящих соединений с данного клиента
     /// </summary>
     public bool Start()
     {
-        throw new NotImplementedException();
+        if (_isStart)
+            return false;
+
+        _isStart = true;
+
+        _receiver = Task.Run(() =>
+        {
+            while (_isStart)
+            {
+                var buffer = new byte[1024];
+                _client.Receive(buffer);
+                _callback(_parser.ParseMessage(Encoding.ASCII.GetString(buffer)));
+            }
+        });
+
+        return true;
     }
 
     /// <summary>
@@ -62,15 +51,40 @@ internal class Connection
     /// <exception cref="NotImplementedException"></exception>
     public bool Stop()
     {
-        throw new NotImplementedException();
+        if (!_isStart)
+            return false;
+
+        _isStart = false;
+        // TODO: Надо подумать, о том, как работать с сокетом
+        _client.Close();
+
+        _receiver.Wait();
+
+        return true;
     }
 
     /// <summary>
     /// Отправить сообщение клиенту
     /// </summary>
     /// <param name="message"> Текст сообщения </param>
-    public bool Send(string message)
+    public bool Send(WebMessage message)
     {
-        throw new NotImplementedException();
+        if (!_isStart)
+            return false;
+
+        Task.Run(() =>
+        {
+            _client.Send(Encoding.ASCII.GetBytes(_parser.CreateMessage(message)));
+        });
+
+        return true;
     }
+
+    private Task _receiver = new(() => { });
+
+    private readonly Socket _client;
+    private readonly Action<WebMessage> _callback;
+    private readonly Parser _parser;
+
+    private bool _isStart;
 }
